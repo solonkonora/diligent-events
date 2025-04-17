17310b22ad57c04d8b1c93eaf0e68f4b5befaf99
solonkonora <nkwadanora@gmail.com>
2025-04-17 18:11:00 +0100

    .

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,4 +1,4 @@
-import EventsPage from "../gallery"
+import EventsPage from "@/components/gallery"
 
 const Gallery = () => {
     return (
@@ -7,3 +7,4 @@ const Gallery = () => {
         </div>
     )
 }
+export default Gallery;
