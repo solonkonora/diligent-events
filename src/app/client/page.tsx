9b785fa252e93a1e6d19e974157961fc7b29921e
nkwadanora <nkwadanora@gmail.com>
2025-08-13 16:45:08 +0100

    substitute loading text with spinner loaders on the the client and admin db pages

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -5,7 +5,32 @@ import { Suspense } from "react";
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading dashboard...</div>}>
+    <Suspense
+      fallback={
+        <div className="flex h-screen items-center justify-center">
+          <svg
+            className="h-10 w-10 animate-spin text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            />
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+            />
+          </svg>
+        </div>
+      }
+    >
       <ClientDashboard />
     </Suspense>
   );
